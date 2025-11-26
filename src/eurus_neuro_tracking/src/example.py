c6ef4d261360de50d4ffda5bfcda4f2634cb9c56
koluiree <nikpoz2005@mail.ru>
2025-11-26 17:50:24 +0300

    добавил оболочку мавлинка и тест на ее работоспособность

---
+++ src/eurus_neuro_tracking/src/example.py
@@ -0,0 +1,12 @@
+from eurus_mavlink.controller import EurusController
+
+controller = EurusController()
+controller.connect()
+# controller.arm()
+
+# controller.takeoff(2)
+
+# controller.land()
+
+# controller.disarm()
+# controller.disconnect()
