8b60a1eed70d1eb7dad40a5f2de7c78d2fc02976
koluiree <nikpoz2005@mail.ru>
2025-12-03 22:56:28 +0300

    обновил сервисы и скрипты запуска к ним

---
+++ scripts/start_mavros.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /opt/ros/humble/setup.bash
+source /home/orangepi/ros2_ws/install/setup.bash
+
+exec ros2 launch /home/orangepi/ros2_ws/src/eurus_edu/eurus_edu/launch/mavros.launch
