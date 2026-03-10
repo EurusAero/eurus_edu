24aa8f6903ca2889eb73e2d66cc9d7bbc2ce0f7c
koluiree <nikpoz2005@mail.ru>
2026-03-10 15:53:53 +0300

    изменил название ROS пакета с нейронкой

--- scripts/start_edu_target_detection.sh
+++ scripts/start_edu_target_detection.sh
@@ -3,4 +3,4 @@
 source /opt/ros/humble/setup.bash
 source /home/orangepi/ros2_ws/install/setup.bash
 
-exec ros2 run edu_target_detection target_detection
+exec ros2 run edu_neuro_detection neuro_detection
